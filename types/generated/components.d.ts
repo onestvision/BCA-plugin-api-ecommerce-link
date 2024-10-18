9bb8952519f81c273d10df12cda6942b147b1c22
Javier Gonzalez <javiergo.2502@gmail.com>
2024-10-18 13:51:21 -0500

    feat: the products order schema was modified

--- types/generated/components.d.ts
+++ types/generated/components.d.ts
@@ -1,5 +1,26 @@
 import type { Schema, Attribute } from '@strapi/strapi';
 
+export interface ProductProducts extends Schema.Component {
+  collectionName: 'components_product_products';
+  info: {
+    displayName: 'products';
+    icon: 'shoppingCart';
+    description: '';
+  };
+  attributes: {
+    product_name: Attribute.String;
+    variation_description: Attribute.String;
+    variation_id: Attribute.String;
+    unit_price: Attribute.Decimal;
+    product_id: Attribute.BigInteger;
+    amount: Attribute.Integer;
+  };
+}
+
 declare module '@strapi/types' {
-  export module Shared {}
+  export module Shared {
+    export interface Components {
+      'product.products': ProductProducts;
+    }
+  }
 }
